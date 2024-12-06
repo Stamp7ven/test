41de364dde342af44828f649609d2b17923c3fa6
Panapol6509650146 <144807621+Panapol6509650146@users.noreply.github.com>
2024-12-06 19:14:31 +0700

    Update user-data.sh

--- user-data.sh
+++ user-data.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 cd home/ubuntu
 
+DOCKER_USERNAME="username"
+
 sudo apt update
 sudo apt install docker.io -y
 
@@ -10,14 +12,14 @@ sudo systemctl enable docker
 sudo usermod -aG docker ubuntu
 newgrp docker
 
-sudo docker pull ${{ secrets.DOCKER_USERNAME }}/petadopt_frontend
-sudo docker run -d -p 3000:3000 --name cs360_frontend_container ${{ secrets.DOCKER_USERNAME }}/petadopt_frontend:latest
+sudo docker pull ${DOCKER_USERNAME}/petadopt_frontend
+sudo docker run -d -p 3000:3000 --name cs360_frontend_container ${DOCKER_USERNAME}/petadopt_frontend:latest
 
-sudo docker pull ${{ secrets.DOCKER_USERNAME }}/petadopt_backend
+sudo docker pull ${DOCKER_USERNAME}/petadopt_backend
 sudo docker run -d -p 1337:1337 --name cs360_backend_container \
-          -e APP_KEYS=${{ secrets.APP_KEYS }} \
-          -e API_TOKEN_SALT=${{ secrets.API_TOKEN_SALT }} \
-          -e ADMIN_JWT_SECRET=${{ secrets.ADMIN_JWT_SECRET }} \
-          -e TRANSFER_TOKEN_SALT=${{ secrets.TRANSFER_TOKEN_SALT }} \
-          -e JWT_SECRET=${{ secrets.JWT_SECRET }} \
-          ${{ secrets.DOCKER_USERNAME }}/petadopt_backend:latest
+          -e APP_KEYS=$(openssl rand -base64 32) \
+          -e API_TOKEN_SALT=$(openssl rand -base64 32) \
+          -e ADMIN_JWT_SECRET=$(openssl rand -base64 32) \
+          -e TRANSFER_TOKEN_SALT=$(openssl rand -base64 32) \
+          -e JWT_SECRET=$(openssl rand -base64 32) \
+          ${DOCKER_USERNAME}/petadopt_backend:latest
