d5489dd03c81961dbeaceadfe335e00c0cfbd23b
Kwandao6509650245 <145100087+Kwandao6509650245@users.noreply.github.com>
2024-12-07 14:20:13 +0700

    Update user-data.sh

--- user-data.sh
+++ user-data.sh
@@ -12,14 +12,14 @@ sudo systemctl enable docker
 sudo usermod -aG docker ubuntu
 newgrp docker
 
-sudo docker pull ${DOCKER_USERNAME}/petadopt_backend
+sudo docker pull ${DOCKER_USERNAME}/strapi-app
 sudo docker run -d -p 1337:1337 --name cs360_backend_container \
           -e APP_KEYS=$(openssl rand -base64 32) \
           -e API_TOKEN_SALT=$(openssl rand -base64 32) \
           -e ADMIN_JWT_SECRET=$(openssl rand -base64 32) \
           -e TRANSFER_TOKEN_SALT=$(openssl rand -base64 32) \
           -e JWT_SECRET=$(openssl rand -base64 32) \
-          ${DOCKER_USERNAME}/petadopt_backend:latest
+          ${DOCKER_USERNAME}/strapi-app:latest
 
-sudo docker pull ${DOCKER_USERNAME}/petadopt_frontend
-sudo docker run -d -p 3000:3000 --name cs360_frontend_container ${DOCKER_USERNAME}/petadopt_frontend:latest
+sudo docker pull ${DOCKER_USERNAME}/react-appv2
+sudo docker run -d -p 3000:3000 --name cs360_frontend_container ${DOCKER_USERNAME}/react-appv2:latest
